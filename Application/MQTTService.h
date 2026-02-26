63c9ffe61df3a33e92bcdf8ac82f8b1b1d94a4e6
anh-tq-huynh <ashley.anhht@gmail.com>
2026-02-26 14:15:24 +0200

    Add modbus as a submodule

--- Application/MQTTService.h
+++ Application/MQTTService.h
@@ -5,7 +5,10 @@
 #ifndef GARAGE_DOOR_MQTT_H
 #define GARAGE_DOOR_MQTT_H
 #include <string>
+
+#include "Countdown.h"
 #include "IPStack.h"
+#include "paho.mqtt.embedded-c/MQTTClient/src/MQTTClient.h"
 using namespace std;
 
 
@@ -15,6 +18,7 @@ class MQTTService
 		string ssid;
 		string pwd;
 		IPStack ipstack;
+		auto client;
 };
 
 
